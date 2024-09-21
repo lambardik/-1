2a796f133dd5e9ab4ed6a56a0970fdaa05fb5f38
Бояр Даниил <BoyarD_82@STUDENTS.MGKE.LOC>
2024-09-21 14:27:05 +0300

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+var_int=10
+var_float=8.4
+var_str="No"
+big_int35=var_int *3.5
+var_float=var_float-1
+var_int/var_float
+big_int35/var_float
+var_str+"No"+3*"Yes"
+print(var_int,var_float,var_str,big_int35,var_int/var_float,big_int35/var_float,var_str+"No"+3*"Yes")
